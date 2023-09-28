a9ea11025cbcd9bdc0b206389bde316297f809f2
jen-buskohl <124739446+jen-buskohl@users.noreply.github.com>
2023-09-28 15:22:29 -0700

    Added code to VS Code and created react integration

---
+++ src/Weather.js
@@ -0,0 +1,65 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+  let weatherData = {
+    city: "Seattle",
+    date: "Tuesday 10:00",
+    description: "Cloudy",
+    imgUrl: "https://ssl.gstatic.com/onebox/weather/64/cloudy.png",
+    temp: "67",
+    humidity: "80",
+    wind: "6",
+  };
+
+  return (
+    <div className="Weather">
+      <form className="mb-3">
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Type a city..."
+              className="form-control search-form"
+              autoComplete="off"
+            />
+          </div>
+          <div className="col-3">
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
+          </div>
+        </div>
+      </form>
+      <div className="overview">
+        <h1>{weatherData.city}</h1>
+        <ul>
+          <li>Last updated: {weatherData.date}</li>
+          <li>{weatherData.description}</li>
+        </ul>
+      </div>
+      <div className="row">
+        <div className="col-6">
+          <div className="d-flex weather-temperature">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png"
+              alt="{weatherData.description}"
+            />
+            <div>
+              <strong></strong>
+              <span className="units">{weatherData.temp}°F</span>
+            </div>
+          </div>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Humidity: {weatherData.humidity}%</li>
+            <li>Wind: {weatherData.wind}m/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
